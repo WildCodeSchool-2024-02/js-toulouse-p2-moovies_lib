071a20c9c27a01359ddcbd19ff6008b9bfd3bf46
MSR3030 <mansour32011@gmail.com>
2024-04-17 16:17:18 +0200

    fix: change title2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <main className="container">
       <header>
-        <h1 className="logo">Romain=Tintin</h1>
+        <h1 className="logo">Romain On a rien sans rien</h1>
       </header>
 
       <nav className="navbar">
