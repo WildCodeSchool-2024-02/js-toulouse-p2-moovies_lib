890223d20898d881cdc2c6c26928973ee5d5225e
Romain <97295039+RomainRG31@users.noreply.github.com>
2024-04-24 12:19:28 +0200

    feat: add-searchbar-with-pagination/miss-some-features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
+import SearchBar from "./components/SearchBar";
 import "./reset.css";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,95 @@
+import { useState, useEffect } from "react";
+
+function SearchBar() {
+  const [searchText, setSearchText] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const token = import.meta.env.VITE_MY_API_TOKEN;
+
+  useEffect(() => {
+    if (searchText !== "") {
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(
+          searchText
+        )}&page=${currentPage}&language=fr&include_adult=false&sort_by=popularity.desc`,
+        options
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSearchResult(data.results);
+          setTotalPages(data.total_pages);
+        })
+        .catch((error) => {
+          console.error("Erreur:", error);
+        });
+    }
+  }, [searchText, currentPage]);
+
+  const handlePrevious = () => {
+    setCurrentPage((prev) => Math.max(prev - 1, 1));
+  };
+
+  const handleNext = () => {
+    setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev));
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        placeholder="Cherche Ton Film"
+      />
+      {searchResult.length > 0 &&
+        searchText !== "" &&
+        searchResult.map((movie) => (
+          <div key={movie.id}>
+            <h2>{movie.title}</h2>
+            <p>{movie.overview}</p>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </div>
+        ))}
+      {searchText === "" && <p>Veuillez rentrer le nom d'un film</p>}
+      {searchText !== "" && searchResult.length === 0 && (
+        <p>Désolé, aucun film ne correspond à ta recherche</p>
+      )}
+      {searchResult.length !== 0 && searchText !== "" && (
+        <div className="container-handleChange">
+          <div className="container-btn">
+            <button
+              type="button"
+              onClick={handlePrevious}
+              disabled={currentPage === 1}
+            >
+              Précédent
+            </button>
+            <button
+              type="button"
+              onClick={handleNext}
+              disabled={currentPage === totalPages}
+            >
+              Suivant
+            </button>
+          </div>
+          <p>
+            Page {currentPage} sur {totalPages}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default SearchBar;
