4f92170210d0841358ce4ef78ff55053afa2b530
PierreVir <pierre.virama@gmail.com>
2024-04-25 16:56:45 +0200

    fix: router is fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,32 +3,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
 import Details from "./pages/Details";
 import Search from "./pages/Search";
 import Filter from "./pages/Filter";
 import { ThemeContextProvider } from "./contexts/ThemeContext";
+import HeaderLayout from "./layouts/HeaderLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/search",
-    element: <Search />,
-  },
-  {
-    path: "/filter",
-    element: <Filter />,
-  },
-  {
-    path: "/details/:filmid",
-    element: <Details />,
+    element: <HeaderLayout />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+      {
+        path: "/filter",
+        element: <Filter />,
+      },
+      {
+        path: "/details/:filmid",
+        element: <Details />,
+      },
+    ],
   },
 ]);
 
